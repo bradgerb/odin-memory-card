7ce5b795d1730b498bfc2ac0a94578b3344729ae
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-11-20 22:30:21 -0500

    Card layout css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <h1>Pokemon Memory Game</h1>
+      <h2>Click the Pokemon you haven't already selected</h2>
+
+      <div className='cardContainer'>
+        <div className="card">card 1</div>
+        <div className="card">card 2</div>
+        <div className="card">card 3</div>
+        <div className="card">card 4</div>
+        <div className="card">card 5</div>
+        <div className="card">card 6</div>
+        <div className="card">card 7</div>
+        <div className="card">card 8</div>
+        <div className="card">card 9</div>
+        <div className="card">card 10</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
