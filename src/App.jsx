6184d0d20c547c180d14fc0ce992238f29c0d93f
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-11-20 22:35:46 -0500

    Add score section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
 
   return (
     <div className='container'>
       <h1>Pokemon Memory Game</h1>
       <h2>Click the Pokemon you haven't already selected</h2>
+
+      <div className="scores">
+        <div className="current">
+          Current Score: {currentScore}
+        </div>
+        <div className="high">
+          High Score: {highScore}
+        </div>
+      </div>
 
       <div className='cardContainer'>
         <div className="card">card 1</div>
