6fa45f210ad1ddefed135ad2a5e4e43d5560b226
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-12-03 18:18:04 -0500

    Clean up comments

--- src/components/imageData.jsx
+++ src/components/imageData.jsx
@@ -1,38 +1,25 @@
-// import { useState, useEffect } from "react";
+async function imageFetch() {
+    const maxID = 151 //Gen 1
+    const ids = new Set();
+    
 
-// function ImageFetcher() {
-    // const [imageData, setImageData] = useState([]);
-    // useEffect(() => {
-        async function imageFetch() {
-            const maxID = 151 //Gen 1
-            const ids = new Set();
-            
+    while(ids.size < 10) {
+        ids.add(Math.ceil(Math.random() * maxID))
+    }
 
-            while(ids.size < 10) {
-                ids.add(Math.ceil(Math.random() * maxID))
+    const pokemon = Promise.all(
+        [...ids].map(async (id) => {
+            const response = await fetch(`https:pokeapi.co/api/v2/pokemon/${id}`);
+            const data = await response.json();
+
+            return {
+                id: data.id,
+                name: data.name,
+                sprite: data.sprites.front_default
             }
-
-            const pokemon = Promise.all(
-                [...ids].map(async (id) => {
-                    const response = await fetch(`https:pokeapi.co/api/v2/pokemon/${id}`);
-                    const data = await response.json();
-                    // console.log(data);
-                    // await setImageData([...imageData, {data}]);
-                    // await console.log(imageData);
-                    return {
-                        id: data.id,
-                        name: data.name,
-                        sprite: data.sprites.front_default
-                    }
-                })
-            )
-            return pokemon
-        }
-
-        // imageFetch(); 
-    // }, [])
-
-    // return { imageData }
-// }
+        })
+    )
+    return pokemon
+}
 
 export { imageFetch }
