981a67683ba71c66070376fd446780adfe5dbafe
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-12-05 18:06:21 -0500

    Add shiny chance

--- src/components/imageData.jsx
+++ src/components/imageData.jsx
@@ -1,6 +1,7 @@
 async function imageFetch() {
     const maxID = 151 //Gen 1
     const ids = new Set();
+    const shinyOdds = 5; //(5%)
     
 
     while(ids.size < 10) {
@@ -11,11 +12,19 @@ async function imageFetch() {
         [...ids].map(async (id) => {
             const response = await fetch(`https:pokeapi.co/api/v2/pokemon/${id}`);
             const data = await response.json();
+            const random = Math.ceil(Math.random() * 100);
+            let sprite;
+
+            if (random >= shinyOdds) {
+                sprite = data.sprites.front_default;
+            } else {
+                sprite = data.sprites.front_shiny;
+            }
 
             return {
                 id: data.id,
                 name: data.name,
-                sprite: data.sprites.front_default
+                sprite: sprite
             }
         })
     )
